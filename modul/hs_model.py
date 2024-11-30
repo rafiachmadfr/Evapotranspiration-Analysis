62661a8769b2f27207246e23f2b2d93a203f3b3a
Rafi Achmadfr <achmadfr123@gmail.com>
2024-11-30 16:26:53 +0700

    update

--- modul/hs_model.py
+++ modul/hs_model.py
@@ -8,8 +8,8 @@ def hs_model (t_max, t_min, t_avg, latitude, input_date):
     KRS = koefisien_empiris(t_rentang)
     Ra = extraterrestrial_radiation(latitude, input_date)
 
-    ET_HS = 0.0135 * KRS * (t_max-t_min)**0.5 * (t_avg + 17.8) * Ra
+    ET_HS_modifikation = 0.0135 * KRS * (t_max-t_min)**0.5 * (t_avg + 17.8) * Ra
     
-    ETo = 0.0023 * (t_avg + 17.8) * ((t_max - t_min) ** 0.5) * Ra
+    ET_HS = 0.0023 * (t_avg + 17.8) * ((t_max - t_min) ** 0.5) * Ra
     
-    return Ra, ET_HS
+    return Ra, ET_HS, ET_HS_modifikation
