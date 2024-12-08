dffc2306d35720f6bb4d51f2a38f1e00b4e33dba
Rafi Achmadfr <achmadfr123@gmail.com>
2024-12-08 08:31:07 +0700

    update

---
+++ variabel/variabel.py
@@ -0,0 +1,19 @@
+# koordinat
+latitude = -8.188
+altitude = 98
+
+# Massa jenis air dalam kg/m^3
+rho_air = 1000
+
+# BC Metode berdasarkan tabel persentase hari terang
+persentase_hari_terang = 0.27
+
+kode_groups = ("A", "B", "C", "D", "E", "F")
+
+sisi_media_tanaman = {
+    'A1' : (12.5,), 'A2' : (12.5,), 'A3' : (12.5,), 'A4' : (12.5,),
+    'B1' : (15.0,), 'B2' : (15.0,), 'B3' : (15.0,), 'B4' : (15.0,),
+    'C1' : (17.5,), 'C2' : (17.5,), 'C3' : (17.5,), 'C4' : (17.5,),
+    'D1' : (20.0,), 'D2' : (20.0,), 'D3' : (20.0,), 'D4' : (20.0,),
+    'E1' : (22.5,), 'E2' : (22.5,), 'E3' : (22.5,), 'E4' : (22.5,),
+    'F1' : (25.0,), 'F2' : (25.0,), 'F3' : (25.0,), 'F4' : (25.0,), }  # Satuan cm
