9be7a7ac6c51b2b6b16e3db9354a9bfd9937d0c5
Rafi Achmadfr <achmadfr123@gmail.com>
2025-01-08 13:41:02 +0700

    update rumus analisis dan prepos

--- modul/gravimetri.py
+++ modul/gravimetri.py
@@ -27,7 +27,7 @@ def gravimetri(berat, waktu, luas, supply_air):
 
     return(perubahan_berat, perubahan_et, et_total)
 
-def gravimetri_numpy(berat, waktu, luas, supply_air):
+def gravimetri_central_diff(berat, waktu, luas, supply_air):
     # Selang waktu antara setiap pengukuran (dalam jam)
     delta_t = waktu[2] - waktu[1]
 
@@ -44,6 +44,28 @@ def gravimetri_numpy(berat, waktu, luas, supply_air):
 
     # Menghitung laju evapotranspirasi
     perubahan_et = supply_air[1:-1] - (perubahan_berat / luas)
+
+    # Menghitung total nilai evapotranspirasi dengan nilai absolut
+    et_total = np.abs(np.nansum(perubahan_et))
+
+    return perubahan_berat, perubahan_et, et_total
+
+def gravimetri_delta(berat, waktu, luas, supply_air):
+    # Selang waktu antara setiap pengukuran (dalam jam)
+
+    # Menghitung perubahan berat dengan menggunakan numpy untuk operasi vektorisasi
+    berat = np.array(berat)
+    waktu = np.array(waktu)
+    supply_air = np.array(supply_air)
+
+    # Menghitung perubahan berat menggunakan vektorisasi
+    dW_dt = berat[1:] - berat[:-1]
+
+    # Menentukan perubahan berat dengan nilai absolut
+    perubahan_berat = np.abs(dW_dt)
+
+    # Menghitung laju evapotranspirasi
+    perubahan_et = supply_air[0:-1] - (perubahan_berat / luas)
 
     # Menghitung total nilai evapotranspirasi dengan nilai absolut
     et_total = np.abs(np.nansum(perubahan_et))
