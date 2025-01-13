3897641d652f090457d1c9f16061b4a9363c8c2a
Rafi Achmadfr <achmadfr123@gmail.com>
2025-01-14 04:05:13 +0700

    revisi capek

--- ModuI/bc_model.py
+++ ModuI/bc_model.py
@@ -1,3 +1,3 @@
 def bc_model (hari_terang, t_avg):
-    ET_BC = 0 + hari_terang * (0.46 * t_avg + 8)
+    ET_BC = hari_terang * (0.46 * t_avg + 8)
     return ET_BC
---
+++ ModuI/koef_empiris.py
@@ -0,0 +1,2 @@
+def koefisien_empiris(t_rentang):
+        return (0.00185 * (t_rentang) ** 2 ) - 0.0433
