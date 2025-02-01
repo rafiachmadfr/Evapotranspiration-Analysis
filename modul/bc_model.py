a56f7a66e1bdaf47155d0958d4fcfbd9da449098
Rafi Achmadfr <achmadfr123@gmail.com>
2025-02-01 21:59:17 +0700

    p

--- modul/bc_model.py
+++
@@ -1,3 +0,0 @@
-def bc_model (hari_terang, t_avg):
-    ET_BC = hari_terang * (0.46 * t_avg + 8)
-    return ET_BC
--- modul/koef_empiris.py
+++
@@ -1,2 +0,0 @@
-def koefisien_empiris(t_rentang):
-        return (0.00185 * (t_rentang) ** 2 ) - 0.0433
